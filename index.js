b66a7cfdab9b2018f3c1c535b0ce5fab9ecff52d
Phing An Hsiao <phinganhsiao@gmail.com>
2021-07-25 22:51:08 -0400

    commit tree 78518bfa61a167d00440af48ceb5a5f2cfee949d

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+// document.addEventListener("DOMContentLoaded", change() {
+//   });
+// function change() {
+// const getID = document.getElementById('text')
+
+// getID.addEventListener('DOMContentLoaded', function(){
+//      getID.textContent = "This is really cool!"
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const getID = document.getElementById('text')
+    getID.textContent = "This is really cool!"
+});
